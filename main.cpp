a2fdfcf13656d801a1e775a8fd25f3baa28e617d
sonal maheshwari <55426531+sonalgaggar@users.noreply.github.com>
2021-06-16 20:12:25 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+  int n;
+  //cout<<"enter size of array: ";
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  
+
+
+int res[n];
+
+ for (int i = 0; i < n; i++) {
+		
+		res[i]=-1; 
+		for (int j = i + 1; j < n + i; j++) {
+			if (arr[i]< arr[j % n]) {
+				
+				res[i]=arr[j%n];
+				break;
+			}
+		}
+		
+	}
+	cout<<"the next greater elements of all array elements: "<<endl;
+ 
+  for (int i = 0; i < n; i++) {
+    cout <<arr[i]<<"---->  " <<res[i] <<endl;
+  }
+ 
+  return 0;
+}
